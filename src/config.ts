6fa992c2e371326e4d7ffc5938c6e771fde7b06d
Alex Robinson <alex@braimler.com>
2022-03-15 13:16:31 -0400

    Document config

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,8 @@ const configKey = process.env.NODE_ENV === 'production'
 const configFile = path.resolve(process.cwd(), `./.${configKey}.env`);
 dotenv.config({ path: configFile});
 
+// Port the server runs on
 export const SERVER_PORT = 3000;
 
+// API key for openweathermap api
 export const WEATHER_APP_ID = process.env.WEATHER_APP_ID;
